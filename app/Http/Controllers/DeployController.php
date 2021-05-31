c35905461b0024cd580e789545a4a492c314d774
Dima <dma.lapsh@mail.ru>
2021-05-31 14:09:47 +0300

    upd

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -9,7 +9,10 @@ class DeployController extends Controller
 
 	public function index(){
 		//берем файлы
-		console_run("git pull");
+		console_run("ls");
+		echo "<br>";
+		console_run("cd /var/www/bot && git pull");
+		echo "<br>";
 //		//перезапускаем очереди
 		console_run("supervisorctl restart laravel-worker");
 
